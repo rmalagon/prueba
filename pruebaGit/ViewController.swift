c13f1cfb4256c2c2e86d1743aaa85077f319ea79
Ricardo Adolfo Malagón Ganado <Ricardo@macbook-pro-de-ricardo.local>
2016-01-24 21:30:28 +0100

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Ricardo Adolfo Malagón Ganado on 24/1/16.
+//  Copyright © 2016 Ricardo Adolfo Malagón Ganado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
